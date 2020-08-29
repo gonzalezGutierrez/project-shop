cea2d99c2c8a41ddca8d8cdd44cf1447464db45b
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-08-29 11:27:52 -0500

    fixed desing url

--- resources/views/layout/admin_layout.blade.php
+++ resources/views/layout/admin_layout.blade.php
@@ -169,7 +169,19 @@
 
     </div>
 
-    
+    <aside id="sidebar-right" class="sidebar-right">
+        <div class="nano">
+            <div class="nano-content">
+                <a href="#" class="mobile-close visible-xs">
+                    Ordenes<i class="fa fa-chevron-right"></i>
+                </a>
+
+                <div class="sidebar-right-wrapper">
+
+                </div>
+            </div>
+        </div>
+    </aside>
 </section>
 
 
