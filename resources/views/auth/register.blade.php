d18fd5ec0b9300de80294ee687ac4de72587f389
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-08-31 15:20:37 -0500

    notifications

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,37 +23,37 @@
                             <div class="col-md-12 form-group">
                                 <input type="text" class="form-control" id="name" name="nombre" value="{{old('nombre')}}" placeholder="Nombre">
                                 @error('nombre')
-                                    <span class="">{{$message}}</span>
+                                    <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
                                 <input type="text" class="form-control" id="apellido" value="{{old('apellido')}}" name="apellido" placeholder="Apellido">
                                 @error('apellido')
-                                    <span class="">{{$message}}</span>
+                                    <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
                                 <input type="text" class="form-control" id="apellido" value="{{old('telefono')}}" name="telefono" placeholder="Telefono">
                                 @error('telefono')
-                                <span class="">{{$message}}</span>
+                                <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
                                 <input type="email" class="form-control" id="email" name="email" value="{{old('email')}}" placeholder="Dirección de correo">
                                 @error('email')
-                                    <span class="">{{$message}}</span>
+                                    <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
                                 <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
                                 @error('password')
-                                    <span class="">{{$message}}</span>
+                                    <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
                                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Confirmar contraseña">
                                 @error('password_confirmation')
-                                <span class="">{{$message}}</span>
+                                <span class="text-danger">{{$message}}</span>
                                 @enderror
                             </div>
                             <div class="col-md-12 form-group">
