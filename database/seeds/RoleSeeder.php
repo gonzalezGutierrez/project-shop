3833722d7ba3896a16fef807c5def9cb1e042f69
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-08-31 13:27:14 -0500

    user seed

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -19,5 +19,14 @@ class RoleSeeder extends Seeder
             'nombre'=>'paypal',
             'estatus'=>'activo'
         ]);
+
+        \App\User::create([
+            'nombre'=>'Guillermo',
+            'apellido'=>'Puente',
+            'telefono'=>'5560111766',
+            'rol_id'=>1,
+            'email'=>'',
+            'password'=>'mydibumedical-administracion',
+        ]);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,13 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\User::create([
-            'nombre'=>'Guillermo',
-            'apellido'=>'Puente',
-            'telefono'=>'5560111766',
-            'rol_id'=>1,
-            'email'=>'',
-            'password'=>'mydibumedical-administracion',
-        ]);
+
     }
 }
