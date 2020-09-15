a6ba1f7c0ab237c5083ecff80587eb8a074f5200
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-09-14 19:01:15 -0500

    latest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,22 @@
     <section class="login_box_area section-margin">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="image-block-content bg-dark-blue inverse-color text-center">
-                        <img src="{{asset('logo.jpg')}}" class="mb-2" style="width: 220px; border-radius: 50%; height: 220px;" alt="">
-                        <h3 class="text-white"> MyDibu Medical</h3>
-                        <h6>Aportando Valor a la Salud</h6>
-                        <a class="btn btn-info btn-sm" href="{{asset('users/create')}}">Crear una cuenta</a>
-                        <a class="btn btn-outline-info btn-sm" href="{{asset('users/create-pymes')}}">Se un miembro PYME</a>
+                <div class="col-lg-6 col-xs-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-body d-flex justify-content-between align-items-center flex-column text-center">
+                            <img src="{{asset('logo.jpg')}}" class="" style="width: 50%; margin: auto; border-radius: 50%; height: 220px;" alt="">
+                            <h3 class="text-white"> MyDibu Medical</h3>
+                            <h6>Aportando Valor a la Salud</h6>
+                            <a class="btn btn-info btn-sm" href="{{asset('users/create')}}">Crear una cuenta</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="login_form_inner">
-                        <h3>Accede con tus credenciales</h3>
-                        <form action="{{{asset('login')}}}" method="POST" class="row login_form" id="contactForm">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h3>Accede con tus credenciales</h3>
+                        </div>
+                        <form action="{{{asset('login')}}}" method="POST" class="row login_form card-body" id="contactForm">
                             @csrf
                             <div class="col-md-12 form-group">
                                 <input type="email" class="form-control" id="name" name="email" value="{{old('email')}}" placeholder="Dirección de correo electronico">
@@ -29,12 +32,6 @@
                                 @error('password')
                                     <span class="text-danger">{{$message}}</span>
                                 @enderror
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <div class="creat_account">
-                                    <input type="checkbox" id="f-option2" name="selector">
-                                    <label for="f-option2">Recordad sesión</label>
-                                </div>
                             </div>
                             <div class="col-md-12 form-group">
                                 <button type="submit" value="submit" class="btn btn-info mb-2 w-100">Acceder</button>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,17 +5,20 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="image-block-content bg-dark-blue inverse-color text-center"">
-                        <img src="{{asset('logo.jpg')}}" class="mb-2" style="width: 220px; border-radius: 50%; height: 220px;" alt="">
-                        <h4>¿Ya tienes una cuenta?</h4>
-                        <a class="btn btn-info btn-sm" href="{{asset('register/pyme')}}">Se un miembro PYME</a>
-                        <a class="btn btn-outline-info btn-sm" href="{{asset('/login')}}">Acceder</a>
+                    <div class="card">
+                        <div class="card-body d-flex justify-content-between align-items-center flex-column text-center">
+                            <img src="{{asset('logo.jpg')}}" class="mb-2" style="width: 220px; border-radius: 50%; height: 220px;" alt="">
+                            <h4>¿Ya tienes una cuenta?</h4>
+                            <a class="btn btn-outline-info btn-sm" href="{{asset('/login')}}">Acceder</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="login_form_inner register_form_inner">
-                        <h3 class="mb-3">Crear una cuenta MyDibu Medical</h3>
-                        <form action="{{asset('users')}}" method="POST" class="row login_form" id="register_form">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h3 class="mb-3">Crear una cuenta MyDibu Medical</h3>
+                        </div>
+                        <form action="{{asset('users')}}" method="POST" class="row login_form card-body" id="register_form">
                             @csrf
                             <div class="col-md-12 form-group">
                                 <input type="text" class="form-control" id="name" name="nombre" value="{{old('nombre')}}" placeholder="Nombre">
