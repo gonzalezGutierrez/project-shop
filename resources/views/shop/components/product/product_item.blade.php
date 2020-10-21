07aa0b931d6f4cf43a48a813be49f388b2f3b752
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-10-20 20:15:34 -0500

    latest

--- resources/views/shop/components/product/product_item.blade.php
+++ resources/views/shop/components/product/product_item.blade.php
@@ -1,4 +1,25 @@
-<div class="col-lg-{{$col_lg}} col-md-{{$col_md}} col-sm-12 col-xs-12 mb-4">
+<div class="col-lg-{{$col_lg}} col-md-{{$col_md}} col-xs-12 col-sm-12 mb-4">
+    <a href="{{asset('/products/'.$product->slug)}}">
+        <div class="card" style="border-top:3px solid #003b77;">
+            <div class="card-header"  style="padding:0px !important;">
+                <img src="{{asset('/'.$product->url_imagen_principal)}}" class="w-100" alt="">
+            </div>
+            <div class="card-body text-center">
+
+                <span class="text-dark mb-2"><span>{{$product->nombre}}</span></span> <br>
+                <span class="text-primary mb-2">${{number_format($product->precio_venta,2,'.',',')}}</span>
+                <form action="{{asset('product_in_shopping_cart')}}" method="POST">
+                    @csrf
+                    <input type="hidden" value="{{$product->id}}" name="producto_id">
+                    <input type="hidden" value="1" name="cantidad">
+                    <button type="submit" class="btn btn-outline-info btn-sm"><i class="fa fa-shopping"></i> Agregar al carrito</button>
+                </form>
+            </div>
+        </div>
+    </a>
+</div>
+
+{{--<div class="col-lg-{{$col_lg}} col-md-{{$col_md}} col-sm-12 col-xs-12 mb-4">
     <a href="{{asset('/products/'.$product->slug)}}">
         <div class="product-wrap">
             <div class="product-caption">
@@ -27,3 +48,4 @@
         </div>
     </a>
 </div>
+--}}
