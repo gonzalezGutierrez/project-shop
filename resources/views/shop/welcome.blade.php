258b1780d986e4308510c6bcf418b83115326f10
Jesús Alberto GONZALEZ GUTIERREZ <163174@ids.upchiapas.edu.mx>
2020-10-31 09:06:23 -0600

    latest

--- resources/views/shop/welcome.blade.php
+++ resources/views/shop/welcome.blade.php
@@ -40,7 +40,7 @@
                         <a href="{{asset('products-category/'.$category->slug)}}">
                             <div class="large-features-box text-center mb-4 " style="padding: 0px !important;" data-aos="fade-up" data-aos-duration="1200">
                                 <div class=" d-table">
-                                    <img src="{{asset($category->url_imagen)}}" style="width: 100% !important; height:360px;" class="img-responsive" alt="">
+                                    <img src="{{asset($category->url_imagen)}}" style="width: 100% !important; height:230px;" class="img-responsive" alt="">
                                 </div>
                                 <h3 class="mt-2"  style="margin-bottom: 5px !important;">{{$category->nombre}}</h3>
                             </div>
